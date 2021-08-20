5df540cb9c4db8fdc9106837994e9c7f41f1eaf7
pauliescanlon <pauliescanlon@gmail.com>
2021-08-20 16:41:14 +0100

    feat: add get-all-comments poll

---
+++ src/pages/comments.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, useState } from 'react'
+import axios from 'axios'
+import { Container, Heading, Grid } from 'theme-ui'
+import { useEffect } from 'react'
+
+const CommentsPage: FunctionComponent = () => {
+  const [comments, setComments] = useState('Loading...')
+
+  const getAllComments = async () => {
+    console.log('getAllComments')
+    try {
+      const response = await axios('/api/get-all-comments')
+
+      setComments(response.data.comments)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getAllComments()
+    const pollInterval = setInterval(() => {
+      getAllComments()
+    }, 60000)
+
+    return () => clearInterval(pollInterval)
+  }, [])
+
+  return (
+    <Container>
+      <Grid
+        sx={{
+          py: 5,
+        }}
+      >
+        <Heading as="h1">Comments</Heading>
+        <pre>{JSON.stringify(comments, null, 2)}</pre>
+      </Grid>
+    </Container>
+  )
+}
+
+export default CommentsPage
