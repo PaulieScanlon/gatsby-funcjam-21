a37da7ee9357b196197d8e1134b440fd8a424adb
pauliescanlon <pauliescanlon@gmail.com>
2021-08-22 13:58:37 +0100

    fix: fix og:image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteMetadata: {
     url: `https://gatsbygroovyanalytics.gatsbyjs.io/`,
     title: `Groovy Analytics`,
-    image: `https://gatsbygroovyanalytics.gatsbyjs.io/images/gatsby-groovy-google-analytics-og-image.jpg`,
+    image: `gatsby-groovy-google-analytics-og-image.jpg`,
     description: `Google Analytics | A #FuncJam site by @PaulieScanlon`,
     language: `en-gb`,
     keywords: [`gatsby`, `gatsby-functions`],
